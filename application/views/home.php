1fd331d1db39ae136e01523d0477413d84ebd9ef
Ryan Sadio <ryan@sadio.ca>
2015-01-28 01:08:53 -0800

    Home, Projects, and Resume init pages.

--- application/views/home.php
+++ application/views/home.php
@@ -26,30 +26,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
-        <div class="container">
+        <div class="container padded">
             <br><br>
-            <img src="../../assets/images/me.jpg" alt="" class="circle responsive-img">
-            <h1 class="header center teal-text text-lighten-2">Ben Soer</h1>
             <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                <img src="../../assets/images/me.jpg" alt="" class="circle responsive-img">
             </div>
+            <h2 class="header center white-text">Ben Soer</h2>
+            <h5 class="header center light">Web Developer</h5>
+            <br>
+            <h5 class="header center light">ben@soer.com</h5>
+            <br><br>
         </div>
     </div>
-    <div class="parallax green"><img src="background1.jpg" alt="Unsplashed background img 2"></div>
+    <div class="parallax midnight_blue"><!--<img src="../../assets/images/background1.jpg" alt="Unsplashed background img 2">--></div>
 </div>
 
 <nav class="" role="navigation">
-    <div class="nav-wrapper">
-        <ul id="nav-mobile" class="center">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="components.html">Components</a></li>
-            <li><a href="javascript.html">JavaScript</a></li>
-        </ul>
+    <div class="container">
+        <div class="nav-wrapper">
+            <div class="row">
+                <div class="col s4 center active">
+                    <a href="/">Home</a>
+                </div>
+                <div class="col s4 center">
+                    <a href="#">Projects</a>
+                </div>
+                <div class="col s4 center">
+                    <a href="#">Resume</a>
+                </div>
+            </div>
+        </div>
     </div>
 </nav>
 
 
-
+<!--CONTENT-->
 <div class="container">
     <div class="section">
 
@@ -85,72 +96,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     </div>
 </div>
+<!--ENDOFCONTENT-->
 
 
-<div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-        <div class="container">
-            <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-            </div>
-        </div>
-    </div>
-    <div class="parallax"><img src="background3.jpg" alt="Unsplashed background img 2"></div>
-</div>
-
-<div class="container">
-    <div class="section">
-
+<footer class="page-footer midnight_blue">
+    <div class="container center">
         <div class="row">
-            <div class="col s12 center">
-                <h3><i class="mdi-content-send brown-text"></i></h3>
-                <h4>Contact Us</h4>
-                <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+
+            <div class="col s4">
+                <p>&nbsp;</p>
             </div>
-        </div>
 
-    </div>
-</div>
-
-
-<div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-        <div class="container">
-            <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+            <div class="col s1">
+                <a href="#"><img src="../../assets/images/linkedin2.png" width="46"></a>
             </div>
-        </div>
-    </div>
-    <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 3"></div>
-</div>
 
-<footer class="page-footer teal">
-    <div class="container">
-        <div class="row">
-            <div class="col l6 s12">
-                <h5 class="white-text">Company Bio</h5>
-                <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+            <div class="col s1">
+                <a href="#"><img src="../../assets/images/social71.png" width="46"></a>
+            </div>
 
+            <div class="col s1">
+                <a href="#"><img src="../../assets/images/social92.png" width="46"></a>
+            </div>
 
+            <div class="col s1">
+                <a href="#"><img src="../../assets/images/share6.png" width="46"></a>
             </div>
-            <div class="col l3 s12">
-                <h5 class="white-text">Settings</h5>
-                <ul>
-                    <li><a class="white-text" href="#!">Link 1</a></li>
-                    <li><a class="white-text" href="#!">Link 2</a></li>
-                    <li><a class="white-text" href="#!">Link 3</a></li>
-                    <li><a class="white-text" href="#!">Link 4</a></li>
-                </ul>
+
+            <div class="col s4">
+                <p>&nbsp;</p>
             </div>
-            <div class="col l3 s12">
-                <h5 class="white-text">Connect</h5>
-                <ul>
-                    <li><a class="white-text" href="#!">Link 1</a></li>
-                    <li><a class="white-text" href="#!">Link 2</a></li>
-                    <li><a class="white-text" href="#!">Link 3</a></li>
-                    <li><a class="white-text" href="#!">Link 4</a></li>
-                </ul>
-            </div>
+
         </div>
     </div>
     <div class="footer-copyright">
--- application/views/resume.php
+++ application/views/resume.php
@@ -1,2 +1,111 @@
 <?php
 /** This is the resume view which displays the users "Resume" page to thier profile site */
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <title>Parallax Template - Materialize</title>
+
+    <!-- CSS  -->
+    <link href="../../assets/materialize/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="../../assets/materialize/css/style_profile.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+
+<div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+        <div class="container padded">
+            <br><br>
+            <div class="row center">
+                <img src="../../assets/images/me.jpg" alt="" class="circle responsive-img">
+            </div>
+            <h2 class="header center white-text">Ben Soer</h2>
+            <h5 class="header center light">Web Developer</h5>
+            <br>
+            <h5 class="header center light">ben@soer.com</h5>
+            <br><br>
+        </div>
+    </div>
+    <div class="parallax midnight_blue"><!--<img src="../../assets/images/background1.jpg" alt="Unsplashed background img 2">--></div>
+</div>
+
+<nav class="" role="navigation">
+    <div class="container">
+        <div class="nav-wrapper">
+            <div class="row">
+                <div class="col s4 center active">
+                    <a href="/">Home</a>
+                </div>
+                <div class="col s4 center">
+                    <a href="#">Projects</a>
+                </div>
+                <div class="col s4 center">
+                    <a href="#">Resume</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+
+
+<!--CONTENT-->
+<div class="container">
+    <div class="section">
+
+        <object width="100%" height="1000px" data="../../assets/pdfs/Untitled.pdf"></object>
+
+    </div>
+</div>
+<!--ENDOFCONTENT-->
+
+
+<footer class="page-footer midnight_blue">
+    <div class="container center">
+        <div class="row">
+
+            <div class="col s4">
+                <p>&nbsp;</p>
+            </div>
+
+            <div class="col s1">
+                <a href="#"><img src="../../assets/images/linkedin2.png" width="46"></a>
+            </div>
+
+            <div class="col s1">
+                <a href="#"><img src="../../assets/images/social71.png" width="46"></a>
+            </div>
+
+            <div class="col s1">
+                <a href="#"><img src="../../assets/images/social92.png" width="46"></a>
+            </div>
+
+            <div class="col s1">
+                <a href="#"><img src="../../assets/images/share6.png" width="46"></a>
+            </div>
+
+            <div class="col s4">
+                <p>&nbsp;</p>
+            </div>
+
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            Made by <a class="brown-text text-lighten-3" href="#">Voodoo Works.</a>
+        </div>
+    </div>
+</footer>
+
+
+<!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="../../assets/materialize/js/materialize.js"></script>
+<script src="../../assets/materialize/js/init_profile.js"></script>
+
+</body>
+</html>
