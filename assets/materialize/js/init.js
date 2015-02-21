bc501b4f52758db858dc34c9a910882f498f75ac
Ryan Sadio <ryan@sadio.ca>
2015-02-21 14:55:34 -0800

    Update Signup input fields

--- assets/materialize/js/init.js
+++ assets/materialize/js/init.js
@@ -19,3 +19,26 @@ $("document").ready(function() {
     $("ul li:first-child div").trigger('click');
   },10);
 });
+
+$('#last_name').on('blur',function(e) {
+    if( document.getElementById('username').value==''
+        && document.getElementById('first_name').value!=''
+        && document.getElementById('last_name').value!='')
+    {
+        // combine firstname and lastname to create username
+        var username = document.getElementById('first_name').value.substr(0,490)
+            + document.getElementById('last_name').value.substr(0,49);
+        username = username.replace(/\s+/g, '');
+        username = username.replace(/\'+/g, '');
+        username = username.replace(/-+/g, '');
+        username = username.toLowerCase();
+        document.getElementById('username').value = username;
+
+        // username label should translate up
+        var label = $('label[for="username"]');
+        label['addClass']('active');
+
+        // username underline should turn green
+        document.getElementById('username').className = "validate valid";
+    }
+});
