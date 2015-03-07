d5abe9ca051f4679c873d83c2a74415f9802c06e
bensoer <ikben@shaw.ca>
2015-03-06 22:47:44 -0800

    added requirements changes for A2 submission

--- application/config/config.php
+++ application/config/config.php
@@ -53,7 +53,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = '/';
 
 /*
 |--------------------------------------------------------------------------
@@ -65,7 +65,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
